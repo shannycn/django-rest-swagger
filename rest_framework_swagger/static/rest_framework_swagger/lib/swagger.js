7517e27e2a3b1b535928b3802047051b4bc0ecad
Shao Lijun <shaolijun@eheartcare.com>
2018-03-27 16:01:22 +0800

    NEW: Fix url visit

--- rest_framework_swagger/static/rest_framework_swagger/lib/swagger.js
+++ rest_framework_swagger/static/rest_framework_swagger/lib/swagger.js
@@ -57,24 +57,24 @@ Object.keys = Object.keys || (function () {
             'constructor'
         ],
         DontEnumsLength = DontEnums.length;
-  
+
     return function (o) {
         if (typeof o != "object" && typeof o != "function" || o === null)
             throw new TypeError("Object.keys called on a non-object");
-     
+
         var result = [];
         for (var name in o) {
             if (hasOwnProperty.call(o, name))
                 result.push(name);
         }
-     
+
         if (hasDontEnumBug) {
             for (var i = 0; i < DontEnumsLength; i++) {
                 if (hasOwnProperty.call(o, DontEnums[i]))
                     result.push(DontEnums[i]);
-            }   
+            }
         }
-     
+
         return result;
     };
 })();
@@ -392,8 +392,11 @@ SwaggerResource.prototype.addApiDeclaration = function(response) {
     this.produces = response.produces;
   if (response.consumes != null)
     this.consumes = response.consumes;
-  if ((response.basePath != null) && response.basePath.replace(/\s/g, '').length > 0)
-    this.basePath = response.basePath.indexOf("http") === -1 ? this.getAbsoluteBasePath(response.basePath) : response.basePath;
+
+  parts = this.basePath.split("/");
+  this.basePath = parts[0] + "//" + parts[2];
+  //if ((response.basePath != null) && response.basePath.replace(/\s/g, '').length > 0)
+  //  this.basePath = response.basePath.indexOf("http") === -1 ? this.getAbsoluteBasePath(response.basePath) : response.basePath;
 
   this.addModels(response.models);
   if (response.apis) {
